9c71fc4a57a6db641ef031ae9b654aee452543e3
pplxlee <erissonleo@outlook.com>
2019-06-03 10:39:50 +0800

    创建VS2015 x64工程

---
+++ sdk_install.bat
@@ -0,0 +1,23 @@
+mkdir C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAV\
+copy /y D:\develop\work_project\QtAV\QtAV\lib_win_x86_64\*Qt*AV*.lib* C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\
+copy /y D:\develop\work_project\QtAV\QtAV\lib_win_x86_64\QtAV1.lib C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\Qt5AV.lib
+copy /y D:\develop\work_project\QtAV\QtAV\lib_win_x86_64\QtAVd1.lib C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\Qt5AVd.lib
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\mkspecs\features\av.prf C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\mkspecs\features\av.prf
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\mkspecs\modules\qt_lib_av*.pri C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\mkspecs\modules\
+copy /y D:\develop\work_project\QtAV\QtAV\bin\Qt*AV*.dll C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\bin\
+mkdir C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAVWidgets\
+copy /y D:\develop\work_project\QtAV\QtAV\lib_win_x86_64\*Qt*AV*.lib* C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\
+copy /y D:\develop\work_project\QtAV\QtAV\lib_win_x86_64\QtAVWidgets1.lib C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\Qt5AVWidgets.lib
+copy /y D:\develop\work_project\QtAV\QtAV\lib_win_x86_64\QtAVWidgetsd1.lib C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\Qt5AVWidgetsd.lib
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\mkspecs\features\avwidgets.prf C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\mkspecs\features\avwidgets.prf
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\mkspecs\modules\qt_lib_avwidgets*.pri C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\mkspecs\modules\
+copy /y D:\develop\work_project\QtAV\QtAV\bin\Qt*AV*.dll C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\bin\
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\..\..\src\QtAV\*.h C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAV\
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\..\..\src\QtAV\QtAV C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAV\
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\..\..\widgets\QtAVWidgets\*.h C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAVWidgets\
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\..\..\widgets\QtAVWidgets\QtAVWidgets C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAVWidgets\
+mkdir C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAV\5.12.2\QtAV\
+xcopy /s /q /y /i D:\develop\work_project\QtAV\QtAV\tools\install_sdk\..\..\src\QtAV\private C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAV\private
+xcopy /s /q /y /i D:\develop\work_project\QtAV\QtAV\tools\install_sdk\..\..\src\QtAV\private C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAV\5.12.2\QtAV\private
+xcopy /s /q /y /i D:\develop\work_project\QtAV\QtAV\bin\QtAV C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\qml\QtAV
+copy /y D:\develop\work_project\QtAV\QtAV\tools\install_sdk\..\..\qml\plugins.qmltypes C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\qml\QtAV\
---
+++ sdk_uninstall.bat
@@ -0,0 +1,13 @@
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\*Qt*AV*
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\Qt5AV.lib
+rd /s /q C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAV
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\mkspecs\features\av.prf
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\mkspecs\modules\qt_lib_av*.pri
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\bin\Qt*AV*.dll
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\*Qt*AV*
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\lib\Qt5AVWidgets.lib
+rd /s /q C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\include\QtAVWidgets
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\mkspecs\features\avwidgets.prf
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\mkspecs\modules\qt_lib_avwidgets*.pri
+del C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\bin\Qt*AV*.dll
+rd /s /q C:\Qt\Qt5.12.2\5.12.2\msvc2015_64\qml\QtAV
